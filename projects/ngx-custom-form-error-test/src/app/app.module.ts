c497778ae0bc9488b307e7b4de58c0511d3542b3
Basu Dev Adhikari <adbasudev54@gmail.com>
2022-06-13 07:28:46 +0545

    Added config to custom form error

--- projects/ngx-custom-form-error-test/src/app/app.module.ts
+++ projects/ngx-custom-form-error-test/src/app/app.module.ts
@@ -12,7 +12,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     NgxCustomFormErrorModule
-
+      .rootConfig(<IErrorConfig>{
+        onTouchedOnly: true,
+        addErrorClassToElement: true,
+        email: 'Please enter a valid email',
+        required: (label: string) => label ? `${label} is required` : `It is required`,
+        pattern: (label: string) => label ? `${label} doesn't match required pattern.` : `Doesn't match required pattern`,
+        minLength: (label: string, data: { minLength: number; }) => label && data ? `${label} should contain at least ${data.minLength} characters.` : `${label} doesn\'t match minimum length criteria.`,
+        maxLength: (label: string, data: { maxLength: number; }) => label && data ? `${label} cannot exceed more than ${data?.maxLength} characters.` : `${label} doesn\'t match maximum length criteria.`
+      })
     ,
     ReactiveFormsModule,
   ],
